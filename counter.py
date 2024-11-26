97885a6a3c6729da3789e86cb4406fece1237e52
Cristian Herrera <cristianhmal@gmail.com>
2024-11-26 00:40:54 -0800

    Initial commit

---
+++ counter.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+# Connect to an SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect('combinations.db')
+
+# Create a cursor object using the cursor() method
+cursor = connc.cursor()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cursor.execute('''CREATE TABLE IF NOT EXISTS stocks (date text, trans text, symbol text, qty real, price real)''')
+
+# cursor.execute("INSERT INTO stocks VALUES ('2006-01-05', 'BUY', 'RHAT', 100, 35.14)")
+
+# Save (commit) the changes
+conn.commit()
+
+
+# Close the connection
+conn.close()
